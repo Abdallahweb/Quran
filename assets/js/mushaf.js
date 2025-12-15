80a54a6bee555eb17769b958414cbba146b61dfd
Abdallahweb <121055533+Abdallahweb@users.noreply.github.com>
2025-12-15 04:56:04 +0300

    Update mushaf.js

--- assets/js/mushaf.js
+++ assets/js/mushaf.js
@@ -1,4 +1,4 @@
-const apiUrl='https://mp3quran.net/api/v3';const endPoint='reciters';const language='ar';const chooseReciters=document.getElementById("chooseReciters");const chooseMoshaf=document.getElementById("chooseMoshaf");const chooseSurah=document.getElementById("chooseSurah");async function getReciters(){const res=await fetch(`${apiUrl}/${endPoint}?language=${language}`);const data=await res.json();data.reciters.forEach(reciter=>{chooseReciters.innerHTML+=`
+const apiUrl='https://mp3quran.net/api/v3';const endPoint='reciters';const language='ar';const AllData=document.getElementById("AllData");const chooseMoshaf=document.getElementById("chooseMoshaf");const chooseSurah=document.getElementById("chooseSurah");const chooseTafsir=document.getElementById("chooseTafsir");async function getReciters(){AllData.innerHTML="";const res=await fetch(`${apiUrl}/${endPoint}?language=${language}`);const data=await res.json();data.reciters.forEach(reciter=>{AllData.innerHTML+=`
            
             <div class="col-lg-4">
 
@@ -10,7 +10,7 @@ ${reciter.name}
 <i class="fa fa-microphone"></i>
 
 </div>  </a>  </div>`})}
-getReciters();async function getMoshaf(reciter){const res=await fetch(`${apiUrl}/${endPoint}?language=${language}&reciter=${reciter.getAttribute("data-reciter-id")}`);const data=await res.json();const moshafs=data.reciters[0].moshaf;chooseSurah.innerHTML="";chooseMoshaf.innerHTML=`<option value="">اختر القراءة</option>`;moshafs.forEach(moshaf=>{chooseMoshaf.innerHTML+=`<option class="option_mushaf" 
+async function getMoshaf(reciter){const res=await fetch(`${apiUrl}/${endPoint}?language=${language}&reciter=${reciter.getAttribute("data-reciter-id")}`);const data=await res.json();const moshafs=data.reciters[0].moshaf;chooseSurah.innerHTML="";chooseMoshaf.innerHTML=`<option value="">اختر القراءة</option>`;moshafs.forEach(moshaf=>{chooseMoshaf.innerHTML+=`<option class="option_mushaf" 
        value="${moshaf.id}"
        data-server = "${moshaf.server}"
         data-surahlist = "${moshaf.surah_list}"
@@ -30,4 +30,16 @@ chooseSurah.innerHTML+=`
        
        `}})})}
 function playSurah(surahMp3){var containerSurah=document.querySelector(".containerSurah");const modelBtn=document.getElementById("modelBtn");var audioPlay=document.getElementById("audioPlay");audioPlay.src=surahMp3.getAttribute("data-surah-id");containerSurah.style.display="block";modelBtn.click();audioPlay.play()}
+function playTafsir(tafsirMp3){var containerSurah=document.querySelector(".containerSurah");const modelBtn=document.getElementById("modelBtn");var audioPlay=document.getElementById("audioPlay");audioPlay.src=tafsirMp3.getAttribute("data-tafsir-id");containerSurah.style.display="block";audioPlay.play()}
 function closeContainer(){var containerSurah=document.querySelector(".containerSurah");var audioPlay=document.getElementById("audioPlay");containerSurah.style.display="none";audioPlay.pause()}
+async function getTafsir(){AllData.innerHTML="";const response=await fetch(`https://www.mp3quran.net/api/v3/tafsir`);const data=await response.json();const soar=data.tafasir.soar;soar.forEach(soars=>{AllData.innerHTML+=`
+           
+            <div class="col-lg-4">
+
+
+   <div class="reciters" onclick="playTafsir(this)"  data-tafsir-id="${soars.url}">
+
+${soars.name}  
+<i class="fa fa-book"></i>
+
+</div>   </div>`})}
